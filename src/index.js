15ed6e7886669392f11d10b38e26efb9d825d1b4
Danylo Bay <danylobay@gmail.com>
2023-03-07 10:19:26 +0100

    update on async/await

--- src/index.js
+++ src/index.js
@@ -6,61 +6,56 @@ import { fetchCountries } from './js/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
 
-refs.searchBoxCountry.addEventListener(
-  'input',
-  debounce(onSearch, DEBOUNCE_DELAY)
-);
-
-function onSearch() {
+const onSearch = async () => {
   const nameOfCountry = refs.searchBoxCountry.value.trim();
   if (nameOfCountry === '') {
     clearMarkupContent();
     return;
   }
 
-  fetchCountries(nameOfCountry)
-    .then(response => {
-      clearMarkupContent();
-      let isNotifyDisplayed = false;
-      response.map(({ name, flags, capital, population, languages }) => {
-        if (response.length > 10 && !isNotifyDisplayed) {
-          Notiflix.Notify.info(
-            'Too many matches found. Please enter a more specific name.'
-          );
-          isNotifyDisplayed = true;
-          return;
-        } else if (response.length > 2 && response.length < 10) {
-          countrySearchMarkup({ name, flags });
-        } else if (response.length === 1) {
-          countryDescriptionMarkup({
-            name,
-            flags,
-            capital,
-            population,
-            languages,
-          });
-        }
-      });
-    })
-    .catch(() => {
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-      clearMarkupContent();
+  try {
+    const response = await fetchCountries(nameOfCountry);
+    clearMarkupContent();
+    let isNotifyDisplayed = false;
+    response.map(({ name, flags, capital, population, languages }) => {
+      if (response.length > 10 && !isNotifyDisplayed) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        isNotifyDisplayed = true;
+        return;
+      } else if (response.length > 2 && response.length < 10) {
+        countrySearchMarkup({ name, flags });
+      } else if (response.length === 1) {
+        countryDescriptionMarkup({
+          name,
+          flags,
+          capital,
+          population,
+          languages,
+        });
+      }
     });
-}
+  } catch (error) {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+    clearMarkupContent();
+  }
+};
 
-function countrySearchMarkup({ name, flags }) {
-  const countryMarkup = `<li class="country-list__item"><img src="${flags.svg}" width="25" height="20" alt="${name.official}"> ${name.official} </li>`;
-  refs.countryList.insertAdjacentHTML('beforeend', countryMarkup);
-}
+const countrySearchMarkup = async ({ name, flags }) => {
+  const countryMarkup =
+    await `<li class="country-list__item"><img src="${flags.svg}" width="25" height="20" alt="${name.official}"> ${name.official} </li>`;
+  return await refs.countryList.insertAdjacentHTML('beforeend', countryMarkup);
+};
 
-function countryDescriptionMarkup({
+const countryDescriptionMarkup = async ({
   name,
   flags,
   capital,
   population,
   languages,
-}) {
-  const countryDescriptionMarkup = `<p class="country-main"><img src="${
+}) => {
+  const countryDescriptionMarkup = await `<p class="country-main"><img src="${
     flags.svg
   }" width="30px" height="25px" alt="${name.official}">${name.official}</p>
         <ul class="country-description">
@@ -68,10 +63,18 @@ function countryDescriptionMarkup({
           <li>Population: ${population}</li>
           <li>languages: ${Object.values(languages)} </li>
           </ul>`;
-  refs.countryInfo.insertAdjacentHTML('beforeend', countryDescriptionMarkup);
-}
+  return await refs.countryInfo.insertAdjacentHTML(
+    'beforeend',
+    countryDescriptionMarkup
+  );
+};
 
-function clearMarkupContent() {
+const clearMarkupContent = () => {
   refs.countryInfo.innerHTML = '';
   refs.countryList.innerHTML = '';
-}
+};
+
+refs.searchBoxCountry.addEventListener(
+  'input',
+  debounce(onSearch, DEBOUNCE_DELAY)
+);
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,17 +1,14 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
-function fetchCountries(name) {
-  return fetch(
-    `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(response => {
-      if (!response.ok) return Promise.reject('Error');
-
-      return response.json();
-    })
-    .catch(error => {
-      console.log(error);
-    });
+async function fetchCountries(name) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
+    );
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export { fetchCountries };
